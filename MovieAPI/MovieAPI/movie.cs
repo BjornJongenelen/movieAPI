aa3624b25286140ea144058b4606e15baed0b4fa
bjorn jongenelen <45263124+BjornJongenelen@users.noreply.github.com>
2020-04-07 21:24:46 +0200

    start van api
    opbouw van de api welke gegevens gaan we bijhouden

---
+++ MovieAPI/MovieAPI/movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI
+{
+    public class movie
+    {
+
+
+        public string Naam { get; set; }
+
+        public string[] Genere { get; set; }
+
+        public int Speelduur { get; set; }
+
+        public int Jaar { get; set; }
+
+        public bool Oscar { get; set; }
+
+        public int AantalOscar { get; set; }
+
+        public string Regiseur { get; set; }
+
+        public string Image { get; set; }
+
+
+
+    }
+}
