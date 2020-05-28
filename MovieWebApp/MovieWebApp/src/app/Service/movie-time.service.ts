3ac312408d1ef5f8ce85c12451588fbcd809041e
Bjorn Jongenelen <bjorn.jongenelen@student.ap.be>
2020-05-28 23:51:06 +0200

    change base url front end
    frotn end kan aan data in de cloud applicatie

--- MovieWebApp/MovieWebApp/src/app/Service/movie-time.service.ts
+++ MovieWebApp/MovieWebApp/src/app/Service/movie-time.service.ts
@@ -9,70 +9,70 @@ export class MovieTimeService {
 
   //get data form the api 
   getFilmData(){
-    return this.http.get<Ifilm>("https://localhost:44356/film")
+    return this.http.get<Ifilm>("https://movietime-mxx2c5xxfq-ew.a.run.app/film")
   }
 
   getSerieData(){
-    return this.http.get<ISerie>("https://localhost:44356/serie")
+    return this.http.get<ISerie>("https://movietime-mxx2c5xxfq-ew.a.run.app/serie")
   }
 
   getActeurData(){
-    return this.http.get<IActeur>("https://localhost:44356/acteur")
+    return this.http.get<IActeur>("https://movietime-mxx2c5xxfq-ew.a.run.app/acteur")
   }
 
   //component maken in api van users
   getUserData(){
-    return this.http.get<Iuser>("https://localhost:44356/user")
+    return this.http.get<Iuser>("https://movietime-mxx2c5xxfq-ew.a.run.app/user")
   }
 
 //////////////////////////////////////////delete data form api 
 
   deleteFilmData(tittle:number){
-    return this.http.delete<Ifilm>(`https://localhost:44356/film/${tittle}`)
+    return this.http.delete<Ifilm>(`https://movietime-mxx2c5xxfq-ew.a.run.app/film/${tittle}`)
   }
 
   deleteSerie(tittle:string){
-    return this.http.delete<ISerie>(`https://localhost:44356/film/${tittle}`)
+    return this.http.delete<ISerie>(`https://movietime-mxx2c5xxfq-ew.a.run.app/serie/${tittle}`)
   }
   
   deleteActeur(naam:string){
-  return this.http.delete<IActeur>(`https://localhost:44356/film/${naam}`)
+  return this.http.delete<IActeur>(`hhttps://movietime-mxx2c5xxfq-ew.a.run.app/acteur/${naam}`)
   }
 
   deleteUser(Username:string){
-    return this.http.delete<Iuser>(`https://localhost:44356/film/${Username}`)
+    return this.http.delete<Iuser>(`https://movietime-mxx2c5xxfq-ew.a.run.app/user/${Username}`)
   }
 ///////////////////////////////////////////sending data 
 
 sendFilmData(film : Ifilm){
-  return this.http.post<Ifilm>(`https://localhost:44356/film`,film)
+  return this.http.post<Ifilm>(`https://movietime-mxx2c5xxfq-ew.a.run.app/film`,film)
 }
 
 sendSerieData(serie: ISerie){
-  return this.http.post<ISerie>(`https://localhost:44356/serie`,serie)
+  return this.http.post<ISerie>(`https://movietime-mxx2c5xxfq-ew.a.run.app/serie`,serie)
 }
 
 sendActorData(acteur : IActeur){
- return this.http.post<IActeur>(`https://localhost:44356/acteur`, acteur) 
+ return this.http.post<IActeur>(`https://movietime-mxx2c5xxfq-ew.a.run.app/acteur`, acteur) 
 }
 
 sendUserData(user : Iuser){
-  return this.http.post<Iuser>(`https://localhost:44356/user`,user)
+  return this.http.post<Iuser>(`https://movietime-mxx2c5xxfq-ew.a.run.app/user`,user)
 }
 
 ///////////////////////////////////////////////////////////////////////////////// zoekfunctie
 
 searchFilm(zoekterm: string){
-  return this.http.get<Ifilm>(`https://localhost:44356/film/search/${zoekterm}`)
+  return this.http.get<Ifilm>(`https://movietime-mxx2c5xxfq-ew.a.run.app/film/search/${zoekterm}`)
 }
 
 searchSerie(zoekterm: string){
-  return this.http.get<ISerie>(`https://localhost:44356/serie/search/${zoekterm}`)
+  return this.http.get<ISerie>(`https://movietime-mxx2c5xxfq-ew.a.run.app/serie/search/${zoekterm}`)
 }
 //////////////////////////////////////////////////update data
 
 BasicLogin(){
-  return this.http.get<Ilogin>(`https://localhost:44356/user/login`)
+  return this.http.get<Ilogin>(`https://movietime-mxx2c5xxfq-ew.a.run.app/user/login`)
 }
 
   constructor(private http:HttpClient) { }
